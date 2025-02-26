3ab1cab28e6a105a7a2e4644341b67e5f9df286c
Amrit <amrit@scalar.com>
2025-02-26 15:16:14 -0800

    feat: somethings working

--- packages/api-reference-react/vite.config.react18.ts
+++ packages/api-reference-react/vite.config.react18.ts
@@ -1,39 +1,20 @@
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-
-import pkg from './package.json'
+import { createViteBuildOptions } from '@scalar/build-tooling'
 
 export default defineConfig({
-  plugins: [react(), dts({ insertTypesEntry: true, rollupTypes: true })],
-  build: {
-    outDir: 'dist/react18',
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: './src/index.ts',
-      name: '@scalar/api-reference-react',
-      formats: ['es'],
-      fileName: 'index',
-    },
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.ts'),
-      },
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        exports: 'named',
-        globals: {
-          'react': 'React',
-          'react-dom': 'react-dom',
+  plugins: [react()],
+  build: createViteBuildOptions({
+    entry: ['src/index.ts'],
+    options: {
+      outDir: 'dist/react18',
+      rollupOptions: {
+        output: {
+          dir: 'dist/react18',
         },
       },
     },
-  },
+  }),
   resolve: {
     alias: {
       'react': 'react18',
